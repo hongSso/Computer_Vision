5db9f234b4a3bd3d29e24110a6e36863a7adf9c6
hongSso <soyunsarang@naver.com>
2022-12-13 03:02:38 +0900

    fix: test

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-
-mat1=np.zeros((3,3))
-mat2=np.ones((3,3))
-mat3=np.identity(3)
-print(mat1)
-print(mat2)
-print(mat3)
--- videocapture.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("error")
-    exit()
-
-print("frame width",int(cap.get(cv.CAP_PROP_FRAME_WIDTH)))
-print("frame height",int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
